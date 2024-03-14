98cd159ca84e67ce7883f4bac85af6f25eed1d17
perebonilado <reradiri@eminenttechnology.com>
2024-03-14 19:53:15 +0100

    banner done

--- src/@modules/home/Banner/index.tsx
+++ src/@modules/home/Banner/index.tsx
@@ -2,17 +2,18 @@ import GradientText from "@/@shared/GradientText";
 import Button from "@/@ui/Button";
 import ArrowDiagonalUpRightIcon from "@/icons/ArrowDiagonalUpRightIcon";
 import React, { FC } from "react";
+import s from './styles.module.css'
 
 const Banner: FC = () => {
   return (
-    <section className="min-h-screen flex flex-col gap-24 text-center items-center justify-center">
+    <section className={`${s.bannerBg} min-h-screen flex flex-col gap-36 text-center items-center justify-center`}>
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-4xl font-bold">
           Restore your device with{" "}
           <GradientText title="iRepair" className="!inline" />
         </h1>
         <div className="bg-[#1363FF] h-[1px] w-[200px] mt-6"></div>
-        <p className="text-lg mt-8 w-full max-w-[400px]">
+        <p className="text-lg mt-8 font-semibold w-full max-w-[400px]">
           Simply hand it to us and we’ll take care of the repair!
         </p>
       </div>
