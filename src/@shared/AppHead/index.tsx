6370e5be44fbc8e515fcdf44fa2ffeddf9430436
perebonilado <reradiri@eminenttechnology.com>
2024-03-18 14:40:51 +0100

    meta adjustment

--- src/@shared/AppHead/index.tsx
+++ src/@shared/AppHead/index.tsx
@@ -16,11 +16,17 @@ const AppHead: FC<Props> = ({ title }) => {
       />
       <meta
         name="keywords"
-        content="phone repair, laptop repair, fix my phone, fix my laptop, phone repairers, laptop repairer, fix my mac, fix my pc, iphone repair, phone fix, iphone fix, samsung repair, my iphone has issues, my phone is bad"
+        content="phone repair, phone repair shops in silver spring  maryland, laptop repair in silver spring maryland, laptop repair, fix my phone, fix my laptop, phone repairers, laptop repairer, fix my mac, fix my pc, iphone repair, phone fix, iphone fix, samsung repair, my iphone has issues, my phone is bad"
       />
-      <meta property="og:title" content="iRepair" />
-      <meta property="og:description" content="Restore your device with iRepair. We provide comprehensive repair services for a wide range of mobile devices and laptops." />
-      <meta property="og:image" content="https://res.cloudinary.com/dt0wfaxft/image/upload/v1710768482/logo_p4dnrn.png" />
+      <meta property="og:title" content="iRepair | Restore your device with iRepair" />
+      <meta
+        property="og:description"
+        content="We provide comprehensive repair services for a wide range of mobile devices and laptops."
+      />
+      <meta
+        property="og:image"
+        content="https://res.cloudinary.com/dt0wfaxft/image/upload/v1710768482/logo_p4dnrn.png"
+      />
     </Head>
   );
 };
